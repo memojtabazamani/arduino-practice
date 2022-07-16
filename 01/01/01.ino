e11a8a864c5f881159f076cf118f7eeb10a2af06
memojtabazamani <memojtabazamani@gmail.com>
2022-07-16 20:51:13 +0430

    first

---
+++ 01/01/01.ino
@@ -0,0 +1,19 @@
+int led = 3;
+char ch;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,  OUTPUT);
+}
+
+void loop() {
+  while(Serial.available())
+  {
+    ch = Serial.read();
+    if (ch == '1') {
+      digitalWrite(led, HIGH); // Turn On LED
+    } else {
+      digitalWrite(led, LOW); // Turn Off LED
+    }
+  }
+}
